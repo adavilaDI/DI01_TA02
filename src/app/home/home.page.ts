c57af3236b586ed35f657b24cafdd28d69fd9f27
adavilaDI <adavila@ikaslea.birt.eus>
2026-09-15 11:24:32 +0200

    primero

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  IonHeader, IonToolbar, IonTitle, IonContent,
-  ToastController
-} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, ToastController, IonFooter, IonItem, IonList, IonLabel, IonButton } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { Elemento } from '../models/elemento.model';
 
@@ -12,10 +9,9 @@ import { Elemento } from '../models/elemento.model';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   //TODO añade los componentes de Ionic y FormsModule a imports
-  imports: [
-    IonHeader, IonToolbar, IonTitle, IonContent, 
-    FormsModule
-  ],
+  imports: [IonLabel, IonList,
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    FormsModule, IonFooter, IonItem, IonButton],
 })
 export class HomePage {
 
@@ -27,18 +23,29 @@ export class HomePage {
   elementos: Elemento[] = [
     // Ejemplo:
     // { id: 1, nombre: 'Elemento 1', descripcion: 'Descripción breve', categoria: 'Cat A' },
+    {id: 1, nombre: "La taberna de Silos", descripcion: "Como un Philip Marlowe de la Edad Media, Gonzalo de Berceo, poeta y copista, se empeña en encontrar al asesino de un monje en el monasterio de Silos.", categoria: "suspense"},
+    {id: 2, nombre: "La elegancia del erizo", descripcion: "En un inmueble burgues de París nada es lo que parece.Dos de sus habitantes esconden un secreto", categoria: "narrativa"},
+    {id: 3, nombre: "Mapa de soledades", descripcion: "Se puede estar solo por muchos motivos. Hay solitarios forzosos y solitarios por eleccion", categoria: "ensayo"},
+    {id: 4, nombre: "Historias de Napoleón, sus esposas y otras mujeres", descripcion: "Las experiencias amorosas de Napoleón con el rigor histórico y la amenidad de un maestro del género", categoria: "historica"},
+    {id: 5, nombre: "Años de mentiras", descripcion: "El genio no se copia y tampoco es posible fabricar un best seller ¿o si?", categoria: "suspense"}
   ];
 
   // TODO (Apartado 3 – Property Binding): Devuelve true si hay elementos en la lista
   get hayElementos(): boolean {
-    return false; //Modificar
+    if (!this.elementosFiltrados){
+      return true; //Modificar
+    }
+    return false;
   }
 
   // TODO (Apartado 3 – Two-way Binding): Filtra los elementos según this.busqueda
   get elementosFiltrados(): Elemento[] {
     // Implementa el filtro (this.elementos.filter): devuelve solo los elementos cuyo nombre
     // incluya el texto de this.busqueda (ignorando mayúsculas/minúsculas -> .toLowerCase())
-    return this.elementos;
+    if (!this.busqueda.trim()){
+      return this.elementos;
+    }
+    return this.elementos.filter(element => element.nombre.toLowerCase().includes(this.busqueda.toLowerCase()));
   }
 
   // TODO Modificar el constructor para inyectar Router y ToastController con inject
@@ -46,6 +53,12 @@ export class HomePage {
 
   // TODO (Apartado 1 + 3 – Event Binding): Mostrar un ion-toast al pulsar el botón
   async mostrarToast(): Promise<void> {
-    // Consulta la teoría: apartado "ion-toast vs ion-alert"    
+    // Consulta la teoría: apartado "ion-toast vs ion-alert" 
+    const toastController = await this.toastController.create({
+      message: 'Hay elementos en la lista',
+      duration: 2000,
+      position: 'bottom'
+    });
+    await toastController.present();
   }
 }
